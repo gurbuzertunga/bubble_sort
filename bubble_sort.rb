b0db5bb2447db85a979b03ae21c27a32c217722e
mahmoud717 <mahmoudmohammad717@gmail.com>
2020-08-14 14:25:37 +0200

    finished the bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,5 @@
 def bub_sort(array)
     n = array.length
-    puts n
     loop do  
         swapped = false
         for x in 0..n-2 do
@@ -8,9 +7,27 @@ def bub_sort(array)
                 array[x] , array[x+1] = array[x+1] , array[x]
                 swapped = true
             end
-              print array
         end
+        print array
        break if  swapped == false 
     end
 end
-bub_sort([11,2,1,23,10, 8 , 3])
+bub_sort([11 ,2 ,1 ,23 ,10 , 8, 3])
+##############################
+##############################
+##############################
+def bub_sort_by(array)
+    n = array.length
+    loop do  
+        swapped = false
+        for x in 0..n-2 do
+            if yield(array[x] , array[x+1]).positive?
+                array[x] , array[x+1] = array[x+1] , array[x]
+                swapped = true
+            end
+        end
+        print array
+       break if  swapped == false 
+    end
+end
+bub_sort_by(["hey", "hi", "hello"]){|left, right| left.length - right.length}
